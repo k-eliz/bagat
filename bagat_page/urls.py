04c73a7e0a9b4a3db03e53c402c70cdb008dc0f4
Elizaveta Kulinich <kulinich.eliz@gmail.com>
2022-11-27 16:18:38 +0300

    adding new page

--- bagat_page/urls.py
+++ bagat_page/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("about_company", views.info, name="info"),
+    path("our_advantages", views.advantages, name="advantages"),
 ]
--- bagat_page/views.py
+++ bagat_page/views.py
@@ -8,3 +8,6 @@ def starting_page(request):
 
 def info(request):
     return render(request, "bagat_page/info.html")
+
+def advantages(request):
+    return render(request, "bagat_page/advantages.html")
